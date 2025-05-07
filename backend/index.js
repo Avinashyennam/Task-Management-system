39d0e570d7f2b7d844a92a62a216a2eab868143a
Avinash <avinashyennam01@gmail.com>
2025-05-07 22:20:40 +0530

    changed backend entry point

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,17 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const app = express();
 const cors = require("cors");
 require("dotenv").config();
 
-const userRoutes = require("../routes/users");
-const taskRoutes = require("../routes/tasks");
+const userRoutes = require("./routes/users");
+const taskRoutes = require("./routes/tasks");
 
-const mongouri = process.env.MONGO_URI;
+const app = express();
 
 let isConnected = false;
-
 async function connectDB() {
   if (isConnected) return;
-  await mongoose.connect(mongouri, {
+  await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
@@ -21,14 +19,17 @@ async function connectDB() {
   console.log("MongoDB connected");
 }
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", userRoutes);
 app.use("/api/tasks", taskRoutes);
 app.get("/", (req, res) => res.send("API is working"));
 
+// Export serverless handler
 module.exports = async (req, res) => {
   await connectDB();
-  app(req, res); // Express handles the request
+  return app(req, res);  // let Express handle it
 };
